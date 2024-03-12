08c1c91d9f1c9e37b0dc4edeb19b07f6c6ceab1c
tinunadno <perminov1901@bk.ru>
2024-03-12 19:19:28 +0300

    added weightMap

--- root/ParticleGravity/Main.java
+++ root/ParticleGravity/Main.java
@@ -9,22 +9,39 @@ import ParticleGravity.Vector.vec2;
 
 public class Main{
     public static void main(String[] args){
-        //star
-        Particle part=new Particle(new vec2(0,0), new vec2(0,0),100, 10, new Color(220,200,255));
+        //star1
+        Particle part=new Particle(new vec2(-200,0), new vec2(0.01,0),100, 10, new Color(220,200,255));
         Scene.addParticle(part);
 
-        //planets
-        for(int i=0;i<1000;i++){
+
+        //galaxy1
+        for(int i=0;i<500;i++){
             double rad=Math.random()*100;
             int clrtemp=255-(int)(rad*2.5);
             int direction=1;
             if(Math.random()>0.5)direction=-1;
-            Particle part2=new Particle(new vec2(0,rad*(direction)), new vec2(direction*Math.sqrt(part.getMass()/rad),0), .00001, 1, new Color(255, clrtemp,clrtemp));
+            Particle part2=new Particle(new vec2(part.getPosition().x(),rad*(direction)), new vec2(direction*Math.sqrt(part.getMass()/rad),0), .00001, 1, new Color(255, clrtemp,clrtemp));
             Scene.addParticle(part2);
         }
 
-        ParticlePlot.setScale(100);
+        //star2
+        Particle part_1=new Particle(new vec2(200,0), new vec2(-.01,0),1, 10, new Color(220,200,255));
+        Scene.addParticle(part_1);
+        //galaxy2
+        for(int i=0;i<500;i++){
+            double rad=Math.random()*100;
+            int clrtemp=255-(int)(rad*2.5);
+            int direction=1;
+            if(Math.random()>0.5)direction=-1;
+            Particle part2=new Particle(new vec2(part_1.getPosition().x(),rad*(direction)), new vec2(direction*Math.sqrt(part_1.getMass()/rad),0), .00001, 1, new Color(255, clrtemp,clrtemp));
+            Scene.addParticle(part2);
+        }
+
+
+
+        ParticlePlot.setScale(200);
         ParticlePlot.setSpeedVectorRender(false);
+        ParticlePlot.setWeightMapRender(true);
         ParticleAnimate.startAnimation();
     }
 }
--- root/ParticleGravity/ParticleRenderAndIteration/ParticlePlot.java
+++ root/ParticleGravity/ParticleRenderAndIteration/ParticlePlot.java
@@ -7,18 +7,43 @@ import ParticleGravity.Vector.vec2;
 public class ParticlePlot {
     private static final int SCREEN_X_SIZE=800;
     private static final int SCREEN_Y_SIZE=400;
+    private static final int WEIGHT_MAP_RESOLUTION=5;
     private static final Screen screen=new Screen(SCREEN_X_SIZE, SCREEN_Y_SIZE, true);
+    private static double[][] weightMap=new double[SCREEN_X_SIZE/WEIGHT_MAP_RESOLUTION][SCREEN_Y_SIZE/WEIGHT_MAP_RESOLUTION];
     private static double scale=100;
     private static boolean speedVectorEnabled=true;
+    private static boolean weightMapEnabled=true;
     //screen y size in units
     public static void plot(){
         screen.refresh();
+        if(weightMapEnabled) {
+            double max = -1;
+            double sum = 1;
+            for (int i = 0; i < weightMap.length; i++) {
+                for (int j = 0; j < weightMap[i].length; j++) {
+
+                    if (max < weightMap[i][j] && weightMap[i][j] < sum) max = weightMap[i][j];
+                    sum += max = weightMap[i][j];
+                }
+            }
+            weightMap[0][0] = 0;
+            weightMap[0][weightMap[0].length - 1] = 0;
+            weightMap[weightMap.length - 1][0] = 0;
+            weightMap[weightMap.length - 1][weightMap[0].length - 1] = 0;
+            for (int i = 0; i < SCREEN_Y_SIZE; i++) {
+                for (int j = 0; j < SCREEN_X_SIZE; j++) {
+                    int value = (int) (weightMap[(int) (j / WEIGHT_MAP_RESOLUTION)][(int) (i / WEIGHT_MAP_RESOLUTION)] * 255 / max);
+                    screen.setPixel(j, i, new Color(value, 0, (255 - value) / 20));
+                }
+            }
+        }
+        weightMap=new double[SCREEN_X_SIZE/WEIGHT_MAP_RESOLUTION][SCREEN_Y_SIZE/WEIGHT_MAP_RESOLUTION];
         for(int i=0;i<Scene.getSize();i++){
             vec2 screenPos=getScreneSpaceCoordinates(Scene.getParticle(i).getPosition());
             if(speedVectorEnabled)
                 PlottingAdditions.drawLine(screenPos, screenPos.add(Scene.getParticle(i).getSpeed()), screen, new Color(255,0,0));
             PlottingAdditions.drawCircle(new vec2(screenPos.x(), screenPos.y()), (int)(Scene.getParticle(i).getSize()*100/scale),screen,Scene.getParticle(i).getColor());
-
+            if(weightMapEnabled)weightMap[(int)(Math.min(Math.max(screenPos.x(), 0)/WEIGHT_MAP_RESOLUTION, weightMap.length-1))][(int)(Math.min(Math.max(screenPos.y(), 0)/WEIGHT_MAP_RESOLUTION, weightMap[0].length-1))]+=Scene.getParticle(i).getMass();
         }
         screen.show();
         try{Thread.sleep(10);}
@@ -30,5 +55,6 @@ public class ParticlePlot {
         return new vec2(xScreenCord, yScreenCord);
     }
     public static void setSpeedVectorRender(boolean val){speedVectorEnabled=val;}
+    public static void setWeightMapRender(boolean val){weightMapEnabled=val;}
     public static void setScale(double Scale){scale=Scale;}
 }
